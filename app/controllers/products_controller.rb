b3ad5e946b6d9ba6c8c7bd3575a352b743d93303
Jon Maciel <joaomaciel.n@gmail.com>
2017-10-24 11:11:54 -0200

    rubocop stuff

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ProductsController < ApplicationController
   layout "hello_world"
   def index
--- app/graphql/awesome_shopping_schema.rb
+++ app/graphql/awesome_shopping_schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 AwesomeShoppingSchema = GraphQL::Schema.define do
   mutation(Types::MutationType)
   query(Types::QueryType)
